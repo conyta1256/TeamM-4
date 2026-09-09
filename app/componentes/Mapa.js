ad8450acd8b439953395d80267bbfbf71d0a3de8
conyta1256 <constanza.gallardo03@alumnos.uach.cl>
2026-09-09 12:07:58 -0300

    Update Mapa.js

--- app/componentes/Mapa.js
+++ app/componentes/Mapa.js
@@ -11,11 +11,6 @@ L.Icon.Default.mergeOptions({
   shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 });
 
-//Delimitación de Valdivia
-const boundsRegion = [
-  [-39.95, -73.35], // Suroeste
-  [-39.70, -73.10]  // Noreste
-];
 
 export default function Mapa() {
   const center = [-39.8142, -73.2459];
@@ -24,18 +19,14 @@ export default function Mapa() {
     <MapContainer
       center={center}
       zoom={13}
-      minZoom={11} // Impide alejar demasiado la cámara fuera de la región
-      maxZoom={18} // Límite de zoom cercano
-      maxBounds={boundsRegion} // Restringe el área visible
-      maxBoundsViscosity={1.0} // Evita que se pueda arrastrar fuera (efecto 'rebote')
-      style={{ height: "1000px", width: "100%", borderRadius: "12px" }}
+      style={{ height: "800px", width: "100%", borderRadius: "12px" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={center}>
-        <Popup>Región delimitada</Popup>
+        <Popup>Punto 1</Popup>
       </Marker>
     </MapContainer>
   );
